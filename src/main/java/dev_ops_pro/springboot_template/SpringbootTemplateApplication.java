ed55deb0010cbb4a4a620356ae39ee8711ebbe51
Jeff Zhang <jeff.zhang@uscis.dhs.gov>
2018-03-26 15:43:02 -0400

    Initial creation: Create Springbot App

---
+++ src/main/java/dev_ops_pro/springboot_template/SpringbootTemplateApplication.java
@@ -0,0 +1,12 @@
+package dev_ops_pro.springboot_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTemplateApplication.class, args);
+	}
+}
---
+++ src/test/java/dev_ops_pro/springboot_template/SpringbootTemplateApplicationTests.java
@@ -0,0 +1,16 @@
+package dev_ops_pro.springboot_template;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootTemplateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
